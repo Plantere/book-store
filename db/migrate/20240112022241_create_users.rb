6fd7648fbf18818ab0ba25a836842db35056f452
Plantere <bfabiobenfica@gmail.com>
2024-01-12 00:25:38 -0300

    Refactor: Using two dots instead of hash rocket

--- db/migrate/20240112022241_create_users.rb
+++ db/migrate/20240112022241_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
-    create_table :users, id: false, :primary_key => :id do |t|
+    create_table :users, id: false, primary_key: :id do |t|
       t.string :id
       t.string :username
       t.string :password
--- db/migrate/20240112031058_create_profiles.rb
+++ db/migrate/20240112031058_create_profiles.rb
@@ -1,6 +1,6 @@
 class CreateProfiles < ActiveRecord::Migration[7.1]
   def change
-    create_table :profiles, :id => false, :primary_key => :id  do |t|
+    create_table :profiles, id: false, primary_key: :id  do |t|
       t.string :id
       t.string :user_id
       t.string :first_name
@@ -11,7 +11,7 @@ class CreateProfiles < ActiveRecord::Migration[7.1]
       t.timestamps
     end
 
-    add_index :profiles, :user_id, :unique => true
+    add_index :profiles, :user_id, unique: true
   end
 
   def down
