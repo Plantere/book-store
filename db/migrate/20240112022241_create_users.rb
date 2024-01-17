f704ec9fa71c99139ca29dd98792d63ffe043403
Plantere <bfabiobenfica@gmail.com>
2024-01-16 21:12:08 -0300

    Refactor: Using "password_digest" instead of only "password" in User Table

--- db/migrate/20240112022241_create_users.rb
+++ db/migrate/20240112022241_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       t.string :username
-      t.string :password
+      t.string :password_digest
       t.string :email
       t.integer :status, :default =>  1
       t.timestamps
--- db/schema.rb
+++ db/schema.rb
@@ -142,7 +142,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_12_173226) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.string "email"
     t.integer "status", default: 1
     t.datetime "created_at", null: false
