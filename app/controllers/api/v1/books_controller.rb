01ae171cf82b8b1a1b440b38b7a8fb25cae7a7cd
Plantere <bfabiobenfica@gmail.com>
2024-01-17 23:41:37 -0300

    Added: Basic function for books model

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::BooksController < ApplicationController
+  def index
+    render json: Book.all
+  end
+
+  def create
+    if Book.exists?(name: params_book[:name], publisher_id: params_book[:publisher_id], author_id: params_book[:author_id])
+      render json: { error: "Book already exists" }, status: :unprocessable_entity
+      return
+    end
+
+    @book = Book.new(params_book)
+    if @book.save
+      render json: { message: "Book created successfully" }, status: :ok
+      return
+    end
+
+    render json: { error: "Failed to create book. Please check the provided data." }, status: :unprocessable_entity
+  end
+
+  private
+  def params_book
+    params.require(:book).permit(:name, :description, :stock_quantity, :publisher_id, :author_id, :genre_id)
+  end
+end
