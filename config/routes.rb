9d0e2a8b22ad11475f18ac77cf22290045c2f6e1
Plantere <bfabiobenfica@gmail.com>
2024-01-17 23:42:07 -0300

    Added: All routes for author, genre, book, and publisher

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,30 @@ Rails.application.routes.draw do
     namespace :v1 do
       post "/register" => "users#register"
       post "/login" => "auth#login"
+
+      scope '/author' do
+        get "/" => "authors#index"
+        post "/" => "authors#create"
+        delete "/:id" => "authors#delete"
+      end
+
+      scope '/genre' do
+        get "/" => "genres#index"
+        post "/" => "genres#create"
+        delete "/:id" => "genres#delete"
+      end
+
+      scope '/book' do
+        get "/" => "books#index"
+        post "/" => "books#create"
+      end
+
+      scope '/publisher' do
+        get "/" => "publishers#index"
+        post "/" => "publishers#create"
+        delete "/:id" => "publishers#delete"
+      end
+
     end
   end
   # Defines the root path route ("/")
