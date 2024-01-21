9c68d17c6cbe87685a07140944cc8ffa43f2aaed
Plantere <bfabiobenfica@gmail.com>
2024-01-21 00:14:54 -0300

    Added: Validation of User model for new items

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :username, :password_digest, :email, presence: true
+  validates :username, uniqueness: true, length: { minimum:4 }
+  validates :email, uniqueness: true, format: URI::MailTo::EMAIL_REGEXP # Ref. https://github.com/ruby/ruby/blob/ruby_2_6/lib/uri/mailto.rb#L55
+  validates :status, numericality: true
+
   has_many :address, dependent: :destroy
   has_many :telephone, dependent: :destroy
   has_many :order, dependent: :destroy
