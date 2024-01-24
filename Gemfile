3bad4ee3fde1156f83cd646d1e4a7535c49bff43
Fábio Torres <bfabiobenfica@gmail.com>
2024-01-24 15:55:26 -0300

    Added: js-routes to javascript get named routes

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'pagy'
 
 gem 'vite_rails'
 
+gem "js-routes"
+
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,8 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # Automatically update js-routes file
+  # when routes.rb is changed
+  config.middleware.use(JsRoutes::Middleware)
 end
