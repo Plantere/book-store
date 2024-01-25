b50a5e625c949856d68eb8e6f165c11f425bb15d
Fábio Torres <bfabiobenfica@gmail.com>
2024-01-25 19:40:49 -0300

    Refactor: Rename Home for remove "Page" prefix from filename

---
+++ app/javascript/pages/Home.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="flex flex-col items-center h-screen justify-center">
+    <h1 class="text-8xl font-bold">Book Store</h1>
+    <h3 class="text-5xl">
+      Coming Soon...
+    </h3>
+  </div>
+</template>
+
+<style>
+</style>
