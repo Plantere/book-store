20102e05f1e50794d63bd281adb6023ef23a7c15
Fábio Torres <bfabiobenfica@gmail.com>
2024-01-26 20:19:36 -0300

    Refactor: Change background color to bg-violet-50

--- app/javascript/components/App.vue
+++ app/javascript/components/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { RouterView } from 'vue-router'
 import Toastfication from './notifications/Toastfication.vue';
+
 </script>
 
 <template>
-  <div class="min-h-screen w-full bg-white">
-    <Toastfication></Toastfication>
+  <div class="min-h-screen w-full bg-violet-50">
+    <Toastfication/>
     <RouterView />
   </div>
 </template>
