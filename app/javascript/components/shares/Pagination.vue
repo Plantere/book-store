6f17cca60902810adefda225da1fb3b33d29585a
Fábio Torres <bfabiobenfica@gmail.com>
2024-01-28 12:23:05 -0300

    Refactor: Pagination with minimumPage show parameter

--- app/javascript/components/shares/Pagination.vue
+++ app/javascript/components/shares/Pagination.vue
@@ -15,18 +15,77 @@ const getPreviousPage = () => (props.currentPage > 1 ? props.currentPage - 1 : 0
 
 const getNextPage = () => (props.currentPage < totalPages ? props.currentPage + 1 : 0);
 
+
+const generatePaginationLinks = (minimumPage: number = 5) => {
+  const generatePageRange = (start: number, end: number) =>
+    Array.from({ length: end - start + 1 }, (_, index) => start + index);
+
+  if (totalPages < minimumPage) {
+    return generatePageRange(1, totalPages);
+  }
+
+  const pagesBeforeCurrent = Math.floor(minimumPage / 2);
+  const pagesAfterCurrent = minimumPage - 1 - pagesBeforeCurrent;
+
+  if (props.currentPage <= pagesBeforeCurrent + 1) {
+    const initialPages = generatePageRange(1, minimumPage - 1);
+    return [...initialPages, '...', totalPages];
+  }
+
+  if (props.currentPage >= totalPages - pagesAfterCurrent) {
+    const finalPages = generatePageRange(totalPages - minimumPage + 2, totalPages);
+    return [1, '...', ...finalPages];
+  }
+
+  const navigationPages:any = [
+    getPreviousPage(),
+    props.currentPage,
+    getNextPage()
+  ];
+
+  if (props.currentPage + pagesAfterCurrent < totalPages) {
+    navigationPages.push('...', totalPages);
+  }
+
+  return navigationPages;
+};
+
 </script>
 
 <template>
-  <ul class="inline-flex -space-x-px text-base h-10">
-    <li class="flex items-center justify-center px-3 h-10 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700">
-      <RouterLink :to="{name: props.routerName, query: {page: getPreviousPage(), search: props.termSearch}}">Previous</RouterLink>
-    </li>
-    <li class="flex items-center justify-center px-3 h-10 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700" v-for="page in totalPages" :key="page">
-      <RouterLink :to="{name: props.routerName, query: { page: page,  search: props.termSearch}}"> {{ page }}</RouterLink>
-    </li>
-    <li class="flex items-center justify-center px-3 h-10 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700">
-      <RouterLink :to="{name: props.routerName, query: {page: getNextPage(), search: props.termSearch}}">Next</RouterLink>
-    </li>
+  <ul class="flex text-base h-10">
+    <RouterLink
+      v-if="getPreviousPage() > 0"
+      :to="{ name: props.routerName, query: { page: getPreviousPage(), search: props.termSearch } }"
+      class="flex items-center justify-center px-3 h-10 ms-0 leading-tight text-gray-500 bg-white rounded-s-lg hover:bg-violet-200 hover:text-violet-600"
+    >
+      Previous
+    </RouterLink>
+
+    <span v-else class="flex items-center justify-center px-3 h-10 ms-0 leading-tight text-gray-500 bg-white rounded-s-lg hover:bg-violet-200 hover:text-violet-600 disabled">Previous</span>
+
+    <div class="flex flex-row" v-for="page in generatePaginationLinks()">
+      <RouterLink
+        v-if="page != '...'"
+        :key="page"
+        :to="{ name: props.routerName, query: { page: page, search: props.termSearch } }"
+        :class="{'text-violet-600 bg-violet-200': currentPage === page}"
+        class="flex items-center justify-center px-3 h-10 leading-tight text-gray-500 bg-white  hover:bg-violet-200 hover:text-violet-600"
+      >
+        {{ page }}
+      </RouterLink>
+
+      <span v-else class="flex items-center justify-center px-3 h-10 leading-tight text-gray-500 bg-white">{{ page }}</span>
+    </div>
+
+
+    <RouterLink
+      v-if="getNextPage() > 0"
+      :to="{ name: props.routerName, query: { page: getNextPage(), search: props.termSearch } }"
+      class="flex items-center justify-center px-3 h-10 leading-tight text-gray-500 bg-white  rounded-e-lg hover:bg-violet-200 hover:text-violet-600"
+    >
+      Next
+    </RouterLink>
+    <span v-else class="flex items-center justify-center px-3 h-10 leading-tight text-gray-500 bg-white  rounded-e-lg hover:bg-gray-100 hover:text-gray-700 disabled">Next</span>
   </ul>
 </template>
