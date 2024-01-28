a3e32e6185deaf499c44db03955c89fa44ee3485
Fábio Torres <bfabiobenfica@gmail.com>
2024-01-28 17:52:01 -0300

    Added: store of cart with localstorage extension

--- app/javascript/components/CardProduct.vue
+++ app/javascript/components/CardProduct.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
+import { useCartStore } from '../stores/cart';
 import Icon from './shares/Icon.vue';
+
+
+const cart = useCartStore();
 
 interface Book {
   image?: string,
@@ -11,6 +15,7 @@ interface Book {
 const props = withDefaults(defineProps<Book>(), {
   image: "https://placehold.co/990x1500",
 })
+
 
 </script>
 
@@ -27,7 +32,7 @@ const props = withDefaults(defineProps<Book>(), {
       <p>
         <span class="text-3xl font-bold">${{ props.price }}</span>
       </p>
-      <a href="#" class="flex items-center justify-center rounded-md bg-violet-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600">
+      <a href="#" @click="cart.addProduct(props.tag)" class="flex items-center justify-center rounded-md bg-violet-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600">
       <Icon name="cart"/>
       Add to cart</a>
     </div>
---
+++ app/javascript/stores/cart.ts
@@ -0,0 +1,58 @@
+import { defineStore } from 'pinia';
+
+interface Product {
+  productId: number;
+  quantity: number;
+}
+
+const STORE_NAME = 'cart'
+
+const getCart = (): Product[] => {
+  const cart = localStorage.getItem(STORE_NAME)
+  return cart ? JSON.parse(cart) : []
+}
+
+const storeLocalStorage = (cartList: Product[]): void => {
+  localStorage.setItem(STORE_NAME, JSON.stringify(cartList))
+}
+
+export const useCartStore = defineStore(STORE_NAME, {
+  state: () => ({
+    cartList: getCart(),
+  }),
+  actions: {
+    addProduct(productId: number): void {
+      let product = this.cartList.find((product: Product) => product.productId === productId)
+
+      if(!product){
+        this.cartList.push({
+          productId: productId,
+          quantity: 1,
+        })
+      }else{
+        product.quantity += 1;
+      }
+
+      storeLocalStorage(this.cartList)
+    },
+    removeProduct(productId: number): void {
+      this.cartList = this.cartList.filter((product: Product) => product.productId !== productId)
+      storeLocalStorage(this.cartList)
+    },
+    decreaseProduct(productId: number): void {
+      let product = this.cartList.find((product: Product) => product.productId === productId)
+      
+      if(!product){
+        return
+      }
+      
+      if(product.quantity <= 1) {
+        this.removeProduct(productId)
+      } else {
+        product.quantity -= 1;
+      }
+
+      storeLocalStorage(this.cartList)
+    },
+  },
+});
