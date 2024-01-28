55797ef9eb9a378fb02a8e193d5ef4fe6066b18d
Fábio Torres <bfabiobenfica@gmail.com>
2024-01-28 15:19:21 -0300

    Refactor: 15 pages as default in pagy

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -17,7 +17,7 @@ Pagy::DEFAULT[:overflow] = :empty_page
 # Instance variables
 # See https://ddnexus.github.io/pagy/docs/api/pagy#instance-variables
 # Pagy::DEFAULT[:page]   = 1                                  # default
-# Pagy::DEFAULT[:items]  = 20                                 # default
+Pagy::DEFAULT[:items]  = 15                                 # default
 # Pagy::DEFAULT[:outset] = 0                                  # default
 
 
