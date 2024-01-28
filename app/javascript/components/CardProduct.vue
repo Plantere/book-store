3d6c7d52bcd1582a2b5b6c3b1994a2f114934307
Fábio Torres <bfabiobenfica@gmail.com>
2024-01-28 18:02:17 -0300

    Fix: Applying classes to show icon of cart

--- app/javascript/components/CardProduct.vue
+++ app/javascript/components/CardProduct.vue
@@ -2,7 +2,6 @@
 import { useCartStore } from '../stores/cart';
 import Icon from './shares/Icon.vue';
 
-
 const cart = useCartStore();
 
 interface Book {
@@ -15,7 +14,6 @@ interface Book {
 const props = withDefaults(defineProps<Book>(), {
   image: "https://placehold.co/990x1500",
 })
-
 
 </script>
 
@@ -33,7 +31,7 @@ const props = withDefaults(defineProps<Book>(), {
         <span class="text-3xl font-bold">${{ props.price }}</span>
       </p>
       <a href="#" @click="cart.addProduct(props.tag)" class="flex items-center justify-center rounded-md bg-violet-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-violet-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-600">
-      <Icon name="cart"/>
+      <Icon name="cart" class="flex items-center w-4 mr-3"/>
       Add to cart</a>
     </div>
   </div>
