79219afe6e32b2d09dd6b2f543e5b3dd67489b40
Fábio Torres <bfabiobenfica@gmail.com>
2024-02-10 22:39:02 -0300

    Refactor: Removing test for validation of transaction and price presence

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -4,16 +4,6 @@ RSpec.describe Book, type: :model do
   describe 'validations' do
     let(:order) {create(:order)}
 
-    it "should not be valid without presence of transaction_id" do
-      order.transaction_id = nil
-      expect(order).to_not be_valid  
-    end
-
-    it "should not be valid without presence of price" do
-      order.price = nil
-      expect(order).to_not be_valid  
-    end
-
     it "should not be valid without presence of address_id" do
       order.address_id = nil
       expect(order).to_not be_valid  
